7d4d56b2bc9c1bb6dc201fb08505d45918e544b3
Alex Somesan <alex.somesan@gmail.com>
2019-02-08 15:16:56 +0100

    Values tuned in-vehicle

--- config.h
+++ config.h
@@ -4,6 +4,9 @@
 // #define ENABLE_FASTADC
 #define FASTADC_WITHOUT_TIMER1
 
+// #define INPUT_TYPE_TEST
+#define INPUT_TYPE_LANCER
+
 #define POT_ADDR AD5274_BASE_ADDR_VDD
 #define TIP_PIN PORTB3  // Arduino pin 11
 #define RING_PIN PORTB2 // Arduino pin 10
@@ -17,34 +20,37 @@
 #endif
 
 #define CMD_DURATION 100
-#define INPUT_SAMPLES 8
-#define INPUT_SAMPLE_INTERVAL 30
+#define INPUT_SAMPLES 4
+#define INPUT_SAMPLE_INTERVAL 10
 #define INPUT_SWAY 3
 
-//Lancer
-// #define INPUT_IDLE_VALUE 656
-
 // Lancer
-// #define IN_VOLUME_UP 372
-// #define IN_VOLUME_DOWN 386
-// #define IN_ARROW_UP 353
-// #define IN_ARROW_DOWN 361
-// #define IN_MODE 346
-// #define IN_VOICE_CND 405
-// #define IN_PHONE_PCKUP 425
-// #define IN_PHONE_HGUP 451
+#ifdef INPUT_TYPE_LANCER
+#define INPUT_IDLE_VALUE 646
+
+#define IN_VOLUME_UP 357
+#define IN_VOLUME_DOWN 371
+#define IN_ARROW_UP 337
+#define IN_ARROW_DOWN 345
+#define IN_MODE 330
+#define IN_VOICE_CMD 390
+#define IN_PHONE_PCKUP 410
+#define IN_PHONE_HGUP 437
+#endif
 
 // Test
+#ifdef INPUT_TYPE_TEST
 #define INPUT_IDLE_VALUE 1023
 
-#define IN_VOLUME_UP 205
+#define IN_VOLUME_UP 207
 #define IN_VOLUME_DOWN 339
-#define IN_ARROW_UP 450
-#define IN_ARROW_DOWN 561
-#define IN_MODE 651
-#define IN_VOICE_CND 732
+#define IN_ARROW_UP 449
+#define IN_ARROW_DOWN 560
+#define IN_MODE 649
+#define IN_VOICE_CMD 732
 #define IN_PHONE_PCKUP 790
 #define IN_PHONE_HGUP 842
+#endif
 
 // Pioneer SPH-DA 120
 #define OUT_SWITCH_SOURCE 13
