30abb51f0f27bdaffaca3adae990b24abf5bfa47
Alex Somesan <alex.somesan@gmail.com>
2021-08-30 21:58:34 +0200

    HostApp: don't close port between connection retries + 0xFAFBFCFD handshake token

--- HostApp/maindialog.cpp
+++ HostApp/maindialog.cpp
@@ -45,51 +45,49 @@ void MainDialog::slotPortChanged(QString port) {
 void MainDialog::slotConnectSerial() {
     qint32 bSpeed = ui->speedComboBox->currentText().toInt();
     QString portName = serPort->portName();
-    bool ack;
 
     qDebug() << "Connecting to" << portName << "at" << bSpeed << "baudrate...";
 
     serPort->setBaudRate(bSpeed);
 
-    ack = serPort->open(QIODevice::ReadWrite);
+    bool openOK = serPort->isOpen();
+    if (!openOK) {
+        openOK = serPort->open(QIODevice::ReadWrite);
+    }
 
-    if (ack) {
+    if (openOK) {
         QByteArray payload((int)4, (char)0);
-        payload[0] = 0xFF;
-        payload[1] = 0xFF;
-        payload[2] = 0xFF;
-        payload[3] = 0xFF;
+        payload[0] = 0xFA;
+        payload[1] = 0xFB;
+        payload[2] = 0xFC;
+        payload[3] = 0xFD;
 
         serPort->write(payload);
         serPort->flush();
         qDebug() << "Asking for handshake...";
 
-        ack = serPort->waitForReadyRead(2000);
+        bool ack = serPort->waitForReadyRead(2000);
         if (ack) {
             QByteArray handshake(5, Qt::Uninitialized);
             handshake.clear();
             handshake.append(serPort->readAll());
-            qDebug() << QString(handshake);
-            ack = QString(handshake).contains("ello");
-            if (ack) {
+            qDebug() << handshake.toHex();
+            QString reply = QString(handshake);
+            if (reply.contains("ello")) {
                 qDebug() << "Successful handshake";
+                ui->statusLabel->setText(QString("Connected to %1 at %2 baudrate").arg(portName).arg(bSpeed));
+                ui->disconnectPushButton->setEnabled(true);
+                ui->connectPushButton->setEnabled(false);
+                emit deviceConnected();
+                connect(serPort, SIGNAL(readyRead()),this, SLOT(slotReadSerial()));
+            } else {
+                ui->statusLabel->setText("Failed to connect");
             }
         } else {
             qDebug() << "Timeout waiting for handshake";
-            serPort->close();
         }
-    }
-    if (ack) {
-        ui->statusLabel->setText(QString("Connected to %1 at %2 baudrate").arg(portName).arg(bSpeed));
-        ui->disconnectPushButton->setEnabled(true);
-        ui->connectPushButton->setEnabled(false);
-        emit deviceConnected();
-        connect(serPort, SIGNAL(readyRead()),this, SLOT(slotReadSerial()));
     } else {
-        serPort->close();
-        ui->statusLabel->setText("Failed to connect");
-        ui->disconnectPushButton->setEnabled(false);
-        ui->connectPushButton->setEnabled(true);
+        qDebug() << "Failed to open serial port";
     }
 }
 
